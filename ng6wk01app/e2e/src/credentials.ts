8116caa7aeb122762ff909ad0c38744cfad722a1
carrascoMDD <carrascomdd@gmail.com>
2018-06-25 02:34:06 +0200

    crendentials

--- ng6wk01app/e2e/src/credentials.ts
+++ ng6wk01app/e2e/src/credentials.ts
@@ -1,6 +1,6 @@
 export class Credentials {
 
-    public static username = "carrascoauth0user01@modeldd.org";
-    public static password = "Au123th456or";
+    public static username = "ng6wk01@modeldd.org";
+    public static password = "ng6WK01auth0";
 
 }
